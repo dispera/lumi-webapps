c39f2141cd096e5ded44d43fd7c164a49887c1d1
Diego Spera <diego@nexiit.com>
2017-01-09 19:24:27 -0200

    Update hello world portuguese message

--- helloapp.py
+++ helloapp.py
@@ -3,4 +3,4 @@ application = Flask(__name__)
 
 @application.route('/')
 def my_blog():
-    return '<h1 style="color:#ffa64d;">Oi, mundo! Tudo beleza!!</h1>'
+    return '<h1 style="color:#ffa64d;">Oi, mundo! That is "Hello, world" in Portuguese :)</h1>'
